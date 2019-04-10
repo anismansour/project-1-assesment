3e7d9e2fdd2960bef2d5c0b1e50809c26c218f2b
Anis Mansour <anismansour@Aniss-MacBook-Pro.local>
2019-04-10 15:47:51 -0700

    done

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ let box;
 const plus = () => {
   box = document.getElementById("textNb").value;
   result = result + Number(box);
+  if (result < 0) {
+    document.getElementById("resultBox").style.color = "red";
+  } else {
+    document.getElementById("resultBox").style.color = "black";
+  }
   document.getElementById("resultBox").innerText = result;
 };
 document.getElementById("buttonPlus").addEventListener("click", plus);
@@ -12,6 +17,12 @@ document.getElementById("buttonPlus").addEventListener("click", plus);
 const min = () => {
   box = document.getElementById("textNb").value;
   result = result - Number(box);
+  if (result < 0) {
+    document.getElementById("resultBox").style.color = "red";
+  } else {
+    document.getElementById("resultBox").style.color = "black";
+  }
+
   document.getElementById("resultBox").innerText = result;
 };
 document.getElementById("buttonMin").addEventListener("click", min);
